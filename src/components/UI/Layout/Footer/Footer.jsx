b43dcc2cb81fa5529576fee520b2f590f68571e6
Daniel Shevtsov <daniel.shevtsov@gmail.com>
2019-03-05 22:32:06 -0600

    Remove extra console.log call

--- src/components/UI/Layout/Footer/Footer.jsx
+++ src/components/UI/Layout/Footer/Footer.jsx
@@ -5,17 +5,14 @@ import * as Styled from './Footer.style'
 
 import Link from '../../Link'
 
-export const Footer = ({ className, name, homeLink, authorName }) => {
-  console.log(name)
-  return (
-    <Styled.Footer className={className}>
-      <Styled.Content>
-        <Styled.Link as={Link} {...homeLink} label={name} icon={null} />
-        <span>{[authorName, '2019'].filter(Boolean).join(', ')}</span>
-      </Styled.Content>
-    </Styled.Footer>
-  )
-}
+export const Footer = ({ className, name, homeLink, authorName }) => (
+  <Styled.Footer className={className}>
+    <Styled.Content>
+      <Styled.Link as={Link} {...homeLink} label={name} icon={null} />
+      <span>{[authorName, '2019'].filter(Boolean).join(', ')}</span>
+    </Styled.Content>
+  </Styled.Footer>
+)
 
 Footer.propTypes = {
   className: PropTypes.string,
